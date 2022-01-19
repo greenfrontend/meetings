9aa68083e4cef7272d018d3da2129aad042090e6
Artem Dunaev <artem.dunaev@dualbootpartners.com>
2022-01-19 21:41:58 +0500

    add event model

---
+++ db/migrate/20220119162909_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title, null: false
+      t.text :description
+      t.string :location, null: false
+      t.date :starttime, null: false
+      t.date :endtime
+      t.string :organizeremail
+      t.string :organizertelegram
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
