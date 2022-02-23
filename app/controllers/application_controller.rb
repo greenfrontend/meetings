4237caad9d8d390bee3c48b221452ff94411b1a4
Artem Dunaev <artem.dunaev@dualbootpartners.com>
2022-02-23 14:00:41 +0500

    add pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
